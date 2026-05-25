00797b1a98469574e898d333caf6c2179ac8ec17
Joseth <elborrachitohg@gmail.com>
2026-05-25 18:25:43 -0500

    caso1

--- casot2.py
+++ casot2.py
@@ -1,1 +1,45 @@
-print("Prueba")
+
+import random
+
+
+def promedioMultiplos3(lst):
+    return recorrerlista(lst, 0, None, None)
+
+
+def recorrerlista(lst, x, menor, mayor):
+
+    if x == len(lst):
+
+        if menor == None:
+            return 0
+        
+        return (menor + mayor) / 2
+
+    else:
+
+        if lst[x] % 3 == 0:
+
+            if menor == None or lst[x] < menor:
+                menor = lst[x]
+
+            if mayor == None or lst[x] > mayor:
+                mayor = lst[x]
+
+        return recorrerlista(lst, x+1, menor, mayor)
+
+
+tam = int(input("Ingrese tamaño del arreglo: "))
+
+lista = []
+
+for i in range(tam):
+
+    num = random.randint(10, 9999)
+
+    lista.append(num)
+
+
+print("Arreglo generado:")
+print(lista)
+
+print("Promedio:", promedioMultiplos3(lista))
